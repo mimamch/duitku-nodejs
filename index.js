658299c5178ba979ba4a2ecee05a254fa14a3806
Muhammad Imam Choirudin <mimamch28@gmail.com>
2022-11-24 20:39:35 +0700

    fix the error

--- index.js
+++ index.js
@@ -16,61 +16,61 @@
 // expiryPeriod = An Integer used for set how long expiry period for virtual account number or retail number.
 
 /**HOW TO USE METHOD REQUEST TRANSACTION
-* requestTransactionObj = An Object data for request to the API 
-* cb = A javascript callback function to catch response data from API */
-const requestTransaction = require('./api/requestTransaction.js')
+ * requestTransactionObj = An Object data for request to the API
+ * cb = A javascript callback function to catch response data from API */
+const requestTransaction = require("./api/requestTransaction.js");
 
 /**HOW TO USE METHOD CHECK TRANSACTION
-* merchantOrderId = A string of your transaction that have been generated on Duitku side, for you to check the transaction status.
-* cb = A javascript callback function to catch response data from API */
-const checkTransaction = require('./api/checkTransaction.js')
+ * merchantOrderId = A string of your transaction that have been generated on Duitku side, for you to check the transaction status.
+ * cb = A javascript callback function to catch response data from API */
+const checkTransaction = require("./api/checkTransaction.js");
 
 /**HOW TO USE METHOD GET PAYMENT METHOD
-* amount = A Number variable, required because it would be check the payment fee 
-* cb = A javascript callback function to catch response data from API */
-const getPaymentMethod = require('./api/getPaymentMethod.js')
+ * amount = A Number variable, required because it would be check the payment fee
+ * cb = A javascript callback function to catch response data from API */
+const getPaymentMethod = require("./api/getPaymentMethod.js");
 
 /**HOW TO USE METHOD CREATE INFVOICE
-* createInvoiceObj = An Object data for request to the API 
-* cb = A javascript callback function to catch response data from API */
-const createInvoice = require('./api/createInvoice.js')
+ * createInvoiceObj = An Object data for request to the API
+ * cb = A javascript callback function to catch response data from API */
+const createInvoice = require("./api/createInvoice.js");
 
 /**HOW TO USE METHOD CALLBACK
-* request = A string data from callback notification to check signature 
-* return object JSON*/
-const callback = require('./api/callback.js')
+ * request = A string data from callback notification to check signature
+ * return object JSON*/
+const callback = require("./api/callback.js");
 
 // OBJECT INSTANCE
-/** REQUEST TRANSACTION CONSTRUCTOR, , , 
+/** REQUEST TRANSACTION CONSTRUCTOR, , ,
  * "paymentAmount": 10000,
  * "paymentMethod": "sample string",
  * "merchantOrderId": "sample string",
  * "productDetails": "sample string",
  */
-const Transaction = require('./model/Transaction.js')
+const Transaction = require("./model/Transaction.js");
 
-/** CREATE INVOICE CONSTRUCTOR, , , 
+/** CREATE INVOICE CONSTRUCTOR, , ,
  * "paymentAmount": 10000,
  * "merchantOrderId": "sample string",
  * "productDetails": "sample string",
  */
-const Invoice = require('./model/Invoice.js')
+const Invoice = require("./model/Invoice.js");
 
-/** CREATE ITEM DETAIL, , , 
+/** CREATE ITEM DETAIL, , ,
  * "price": 10000,
  * "name": "sample string",
  * "qty": 1,
  */
-const ItemDetail = require('./model/itemDetail.js')
+const ItemDetail = require("./model/ItemDetail.js");
 
 const duitku = {
-    requestTransaction,
-    checkTransaction,
-    getPaymentMethod,
-    createInvoice,
-    callback,
-    Transaction,
-    Invoice,
-    ItemDetail
-}
+  requestTransaction,
+  checkTransaction,
+  getPaymentMethod,
+  createInvoice,
+  callback,
+  Transaction,
+  Invoice,
+  ItemDetail,
+};
 module.exports = duitku;
